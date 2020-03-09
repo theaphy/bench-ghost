86eac2e102181c625ea6b67bdd53ac6049243a13
theaphy <theheaphy@gmail.com>
2020-03-08 23:22:19 -0700

    about page almost done

--- content/themes/bench/assets/js/slider.js
+++ content/themes/bench/assets/js/slider.js
@@ -1,10 +1,15 @@
 const arrows = document.getElementsByClassName("arrow");
 const slider = document.getElementById("testimonial-slider_inner");
+let sliderChildren = document.getElementsByClassName("our-work_slider");
+const sliderKidsArray = [...sliderChildren];
+console.log(sliderKidsArray);
+
+let currentSlide = 1;
 
 for (let arrow of arrows) {
     arrow.addEventListener('click', (e) => {
-        console.log(e.target.id);
-        console.log(slider.style.transform = "translateX(-1210px)");
+        console.log(e.target.dataset);
+        
     });
 }
 
